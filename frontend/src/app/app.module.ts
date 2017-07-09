8d73a042df65979c36bba4f456ef4d2beb067469
ThisIsTyDell <milleb94@gmail.com>
2017-07-09 12:21:05 -0500

    import FormsModule, HttpModule, PhotoList component, and Photo Service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home.component';
+import { PhotoList } from './components/photo-list.component';
+
+import { PhotoService } from './services/photo.service';
+
 @NgModule({
   declarations: [
     AppComponent,
