67bc0233d37cd443db316cf9e21a3bc3b6262da8
ThisIsTyDell <milleb94@gmail.com>
2017-07-09 16:45:03 -0500

    add commentid and set to generate auto

--- Photable-Backend/src/main/java/com/tymiller/model/Comment.java
+++ Photable-Backend/src/main/java/com/tymiller/model/Comment.java
@@ -1,8 +1,14 @@
 package com.tymiller.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Comment {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long commentId;
 }
