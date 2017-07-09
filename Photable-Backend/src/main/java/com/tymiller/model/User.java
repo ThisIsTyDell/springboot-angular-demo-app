845b5dc9714dc0f65ad5a5a5f9d6123894ecfb61
ThisIsTyDell <milleb94@gmail.com>
2017-07-09 16:40:51 -0500

    add getters and setters for fields

--- Photable-Backend/src/main/java/com/tymiller/model/User.java
+++ Photable-Backend/src/main/java/com/tymiller/model/User.java
@@ -36,4 +36,69 @@ public class User {
 	
 	@ManyToMany
 	private List<Photo> likedPhotoList;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public List<Photo> getPhotoList() {
+		return photoList;
+	}
+
+	public void setPhotoList(List<Photo> photoList) {
+		this.photoList = photoList;
+	}
+
+	public List<Photo> getLikedPhotoList() {
+		return likedPhotoList;
+	}
+
+	public void setLikedPhotoList(List<Photo> likedPhotoList) {
+		this.likedPhotoList = likedPhotoList;
+	}
+		
 }
