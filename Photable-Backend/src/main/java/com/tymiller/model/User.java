7a370ca44e5ae29abdf55c1b9a6891d1b36ad356
ThisIsTyDell <milleb94@gmail.com>
2017-07-09 16:37:54 -0500

    add json annotation

--- Photable-Backend/src/main/java/com/tymiller/model/User.java
+++ Photable-Backend/src/main/java/com/tymiller/model/User.java
@@ -1,13 +1,19 @@
 package com.tymiller.model;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class User {
@@ -22,4 +28,8 @@ public class User {
 	
 	@CreationTimestamp
 	private Date created;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JsonManagedReference
+	private List<Photo> photoList;
 }
