cc5dfcd8436e4590395ee8217c31c046481523da
ThisIsTyDell <milleb94@gmail.com>
2017-07-09 15:28:38 -0500

    new spring boot backend

---
+++ Photable-Backend/src/main/java/com/tymiller/PhotableBackendApplication.java
@@ -0,0 +1,12 @@
+package com.tymiller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotableBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotableBackendApplication.class, args);
+	}
+}
---
+++ Photable-Backend/src/test/java/com/tymiller/PhotableBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tymiller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotableBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
