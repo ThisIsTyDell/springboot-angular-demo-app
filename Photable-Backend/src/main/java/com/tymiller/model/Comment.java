e52b6c7d3b8a49dc810f767870d3cc8a8d5fb1ef
ThisIsTyDell <milleb94@gmail.com>
2017-07-09 16:48:37 -0500

    add getters and setters for comment fields

--- Photable-Backend/src/main/java/com/tymiller/model/Comment.java
+++ Photable-Backend/src/main/java/com/tymiller/model/Comment.java
@@ -21,5 +21,36 @@ public class Comment {
 	private Photo photo;
 	private Long photoId;
 	private String userName;
+	public Long getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(Long commentId) {
+		this.commentId = commentId;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Photo getPhoto() {
+		return photo;
+	}
+	public void setPhoto(Photo photo) {
+		this.photo = photo;
+	}
+	public Long getPhotoId() {
+		return photoId;
+	}
+	public void setPhotoId(Long photoId) {
+		this.photoId = photoId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
 	
 }
