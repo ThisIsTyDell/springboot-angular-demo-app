1701b71ad3f3c75b63be75adef4e15c442a52e31
ThisIsTyDell <milleb94@gmail.com>
2017-07-09 10:04:16 -0500

    fix asset pipeline

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './css/semantic.css', './css/main.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'app';
