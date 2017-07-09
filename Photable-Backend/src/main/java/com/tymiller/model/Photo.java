8554664464e2a3a83b71293594457984085cae4b
ThisIsTyDell <milleb94@gmail.com>
2017-07-09 15:46:51 -0500

    generate timestamp

--- Photable-Backend/src/main/java/com/tymiller/model/Photo.java
+++ Photable-Backend/src/main/java/com/tymiller/model/Photo.java
@@ -1,9 +1,13 @@
 package com.tymiller.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 public class Photo {
@@ -15,4 +19,7 @@ public class Photo {
 	private String title;
 	private String description;
 	private String imageName;
+	
+	@CreationTimestamp
+	private Date created;
 }
