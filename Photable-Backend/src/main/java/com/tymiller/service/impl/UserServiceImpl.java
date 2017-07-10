cecdb29227d538885e37235f5a03714bf98fd76d
ThisIsTyDell <milleb94@gmail.com>
2017-07-09 21:28:03 -0500

    create user service implementation

---
+++ Photable-Backend/src/main/java/com/tymiller/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.tymiller.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.tymiller.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+}
