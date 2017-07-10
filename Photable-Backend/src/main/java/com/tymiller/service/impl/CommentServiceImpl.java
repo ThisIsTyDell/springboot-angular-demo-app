a0fc3730c358b15463de5f9a819e6e62151d0048
ThisIsTyDell <milleb94@gmail.com>
2017-07-10 15:07:13 -0500

    mark as service and autowire commentdao

--- Photable-Backend/src/main/java/com/tymiller/service/impl/CommentServiceImpl.java
+++ Photable-Backend/src/main/java/com/tymiller/service/impl/CommentServiceImpl.java
@@ -2,24 +2,28 @@ package com.tymiller.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.tymiller.model.Comment;
 import com.tymiller.service.CommentService;
 
+@Service
 public class CommentServiceImpl implements CommentService {
 
-	@Override
+	@Autowired
+	private CommentDao commentDao;
+	
 	public Comment save(Comment comment) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public List<Comment> findByPhotoId(Long photoId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Comment findOne(Long commentId) {
 		// TODO Auto-generated method stub
 		return null;
