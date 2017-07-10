e98035f80b4f52c93424740c193482e74b37888d
ThisIsTyDell <milleb94@gmail.com>
2017-07-09 21:22:14 -0500

    add user service, login method, and register user

--- Photable-Backend/src/main/java/com/tymiller/controller/UserController.java
+++ Photable-Backend/src/main/java/com/tymiller/controller/UserController.java
@@ -1,10 +1,23 @@
 package com.tymiller.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tymiller.model.User;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
+	
+	@Autowired
+	private UserService userService;
+	
+	public String login() {}
+	
+	@RequestMapping(value="/register" method = RequestMethod.POST)	
+	public User registerUser(@RequestBody User user) {
+		
+	}
 }
