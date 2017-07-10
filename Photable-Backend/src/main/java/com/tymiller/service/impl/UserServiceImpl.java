f4959d1b7cf00686585b3f3dc501aff941a68ea7
ThisIsTyDell <milleb94@gmail.com>
2017-07-09 21:32:27 -0500

    import autowired

--- Photable-Backend/src/main/java/com/tymiller/service/impl/UserServiceImpl.java
+++ Photable-Backend/src/main/java/com/tymiller/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.tymiller.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tymiller.model.User;
