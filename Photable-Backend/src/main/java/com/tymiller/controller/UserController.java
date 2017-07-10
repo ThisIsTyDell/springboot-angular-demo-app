790aaf4ab5d9b4b7ecdc84362e1445066121c7a4
ThisIsTyDell <milleb94@gmail.com>
2017-07-09 21:45:10 -0500

    import user service

--- Photable-Backend/src/main/java/com/tymiller/controller/UserController.java
+++ Photable-Backend/src/main/java/com/tymiller/controller/UserController.java
@@ -3,9 +3,11 @@ package com.tymiller.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tymiller.model.User;
+import com.tymiller.service.UserService;
 
 @RestController
 @RequestMapping("/user")
