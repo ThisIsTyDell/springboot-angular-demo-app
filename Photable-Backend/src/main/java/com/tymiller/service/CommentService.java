5d0dbae1912f1046fa95684a0817a3b4f55ee11e
ThisIsTyDell <milleb94@gmail.com>
2017-07-10 15:16:21 -0500

    fix error

--- Photable-Backend/src/main/java/com/tymiller/service/CommentService.java
+++ Photable-Backend/src/main/java/com/tymiller/service/CommentService.java
@@ -1,5 +1,13 @@
 package com.tymiller.service;
 
-public interface CommentService {
+import java.util.List;
 
+import com.tymiller.model.Comment;
+
+public interface CommentService {
+	Comment save(Comment comment);
+	
+	List<Comment> findByPhotoId(Long photoId);
+	
+	Comment findOne(Long commentId);
 }
