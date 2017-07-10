b2d64535e02fad075328743df199328b231ea605
ThisIsTyDell <milleb94@gmail.com>
2017-07-10 11:28:07 -0500

    add imports

--- Photable-Backend/src/main/java/com/tymiller/controller/UserResource.java
+++ Photable-Backend/src/main/java/com/tymiller/controller/UserResource.java
@@ -1,8 +1,12 @@
 package com.tymiller.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tymiller.model.User;
 import com.tymiller.service.UserService;
 
 @RestController
