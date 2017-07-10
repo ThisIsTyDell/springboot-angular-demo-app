b6c99545784d7d302da3ebd82eeda5ebbb89549a
ThisIsTyDell <milleb94@gmail.com>
2017-07-10 15:06:04 -0500

    implement comment service

---
+++ Photable-Backend/src/main/java/com/tymiller/service/impl/CommentServiceImpl.java
@@ -0,0 +1,28 @@
+package com.tymiller.service.impl;
+
+import java.util.List;
+
+import com.tymiller.model.Comment;
+import com.tymiller.service.CommentService;
+
+public class CommentServiceImpl implements CommentService {
+
+	@Override
+	public Comment save(Comment comment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Comment> findByPhotoId(Long photoId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comment findOne(Long commentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
