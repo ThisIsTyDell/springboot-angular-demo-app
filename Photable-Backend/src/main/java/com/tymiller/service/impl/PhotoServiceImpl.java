d6a88f7f36b4a27c997fd2c0b1bd73d2628c994c
ThisIsTyDell <milleb94@gmail.com>
2017-07-10 12:38:19 -0500

    create photo service implementtation

---
+++ Photable-Backend/src/main/java/com/tymiller/service/impl/PhotoServiceImpl.java
@@ -0,0 +1,35 @@
+package com.tymiller.service.impl;
+
+import java.util.List;
+
+import com.tymiller.model.Photo;
+import com.tymiller.model.User;
+import com.tymiller.service.PhotoService;
+
+public class PhotoServiceImpl implements PhotoService {
+
+	@Override
+	public Photo save(Photo photo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Photo> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Photo> findByUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Photo findByPhotoId(Long photoId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
