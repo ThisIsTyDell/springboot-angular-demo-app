1c437a19f2492e2d3b43349526df5b2276b5f51b
ThisIsTyDell <milleb94@gmail.com>
2017-07-10 12:07:21 -0500

    add photo controller

---
+++ Photable-Backend/src/main/java/com/tymiller/controller/PhotoController.java
@@ -0,0 +1,17 @@
+package com.tymiller.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/photo")
+public class PhotoController {
+
+	@Autowired
+	private PhotoService photoService;
+	
+	@RequestMapping("/allPhotos")
+	public List<Photo> getAllPhotos() {
+		return photoService.findAll();
+	}
+}
