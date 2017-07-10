dc342be40c276c394287700cfa1048407bb2a8a1
ThisIsTyDell <milleb94@gmail.com>
2017-07-10 14:30:02 -0500

    fix file seperator

--- Photable-Backend/src/main/java/com/tymiller/controller/PhotoResource.java
+++ Photable-Backend/src/main/java/com/tymiller/controller/PhotoResource.java
@@ -37,10 +37,7 @@ public class PhotoResource {
 		String fileName = multipartFile.getOriginalFilename();
 		imageName=fileName;
 		
-		String path = new File("target" + File.separator
-							  + "classes" + File.separator
-							  +"static" + File.separator
-							  + "images").getAbsolutePath() + File.separator + fileName;
+		String path = new File("target/classes/static/images").getAbsolutePath()+"/"+fileName;
 		try {
 			multipartFile.transferTo(new File(path));
 			System.out.println(path);
