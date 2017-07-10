b057750a1651e895dde2ca24354a863915ab3182
ThisIsTyDell <milleb94@gmail.com>
2017-07-10 12:35:36 -0500

    create photo service

---
+++ Photable-Backend/src/main/java/com/tymiller/service/PhotoService.java
@@ -0,0 +1,16 @@
+package com.tymiller.service;
+
+import java.util.List;
+
+import com.tymiller.model.Photo;
+import com.tymiller.model.User;
+
+public class PhotoService {
+	Photo save(Photo photo);
+	
+	List<Photo> findAll();
+	
+	List<Photo> findByUser(User user);
+	
+	Photo findByPhotoId(Long photoId);
+}
