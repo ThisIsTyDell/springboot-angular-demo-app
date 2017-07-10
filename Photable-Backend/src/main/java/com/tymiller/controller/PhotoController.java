1870eb62fff8427816690c620ee139ff9ef0af1f
ThisIsTyDell <milleb94@gmail.com>
2017-07-10 12:48:52 -0500

    fix imports

--- Photable-Backend/src/main/java/com/tymiller/controller/PhotoController.java
+++ Photable-Backend/src/main/java/com/tymiller/controller/PhotoController.java
@@ -1,7 +1,13 @@
 package com.tymiller.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tymiller.model.Photo;
+import com.tymiller.service.PhotoService;
 
 @RestController
 @RequestMapping("/photo")
